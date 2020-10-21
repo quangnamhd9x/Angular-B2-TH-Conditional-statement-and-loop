e8b98374645ad2e661e54581af4106d489b10315
QuangNam <quangnamhd9x@gmail.com>
2020-10-21 08:59:39 +0700

    first commit

---
+++ demo.ts
@@ -0,0 +1,13 @@
+var c:boolean = true;
+
+if (c){     // bang voi c == true
+    console.log("ok");
+} else {
+    console.log("No");
+}
+
+console.log("-----for-----");
+
+for (var i = 0; i <= 5; i++){
+    console.log("For " + i);
+}
